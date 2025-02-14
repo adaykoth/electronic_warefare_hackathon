e4ecd0745a3e0e43ca83f999a6e02427385fbbb8
LukHah <57041862+LukHah@users.noreply.github.com>
2025-02-14 13:47:06 +0100

    added plot values function

--- parse_data.py
+++ parse_data.py
@@ -1,14 +1,36 @@
 import polars as pl
 from pathlib import Path
 
+import matplotlib.pyplot as plt
+
 def load_window(file: Path) -> pl.DataFrame:
     df = pl.read_ipc(file)
     return df
 
+def plot_values(df: pl.DataFrame, emitter: int, output_folder: str):
+    filtered_df = df.filter(df["emitter"] == emitter)
+
+    for col in set(filtered_df.columns) - {"emitter", "arrival_time"}:
+
+        time_list = filtered_df["arrival_time"].to_list()
+        y_list = filtered_df[col].to_list()
+
+        plt.figure()
+        plt.plot(time_list, y_list, marker="o", linestyle="-")
+        plt.xlabel("Time")
+        plt.ylabel(col)
+        plt.title(f"Emitter {emitter}: {col} vs Time")
+        plt.tight_layout()
+        
+        os.makedirs(output_folder, exist_ok=True)   
+        file_name = os.path.join(output_folder, f"emitter_{emitter}_{col}.png")
+        plt.savefig(file_name)
+        plt.close()
+
 
 if __name__ == "__main__":
     import sys
-    
+
     file = Path(sys.argv[1])
     df = load_window(file)
 
